f0472d27856aceae55e5e32961ccd9076af75ffe
Darshit Raj <darshitrajonline@gmail.com>
2024-11-17 20:39:17 +0530

    styled more

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
     <img src="./images/book-4986.png">
     <br>
     <br>
-  The Book Exchange
+  EzBookXChange
     <br>
   <br>
 </h1>
@@ -13,19 +13,19 @@
 </p>
 
 ## Description
-<h4><strong>The Book Exchange is a one stop book trading platform, allowing users to not only search for their favorite books by ISBN, but also intereact with other users to trade books with each other.</strong></h4>
+<h4><strong>EzBookXChange is a one stop book trading platform, allowing users to not only search for their favorite books by ISBN, but also intereact with other users to trade books with each other.</strong></h4>
 
 ## Features
 
 ### Login & Register Functionality
-Leveraging the relationality of PostgreSQL, the Book Exchange has the ability to create new users, and also allow those users to have books that correspond with their user profile. 
+Leveraging the relationality of PostgreSQL, EzBookXChange has the ability to create new users, and also allow those users to have books that correspond with their user profile. 
 
 <p align="center">
 <img src="./images/register.png" />
 </p>
 
 ### Add Favorite Books By ISBN
-Want to add your favorite book? Simply find your favorite reading material's ISBN and add it to the Book Exchange to have a customized platform to note the books you currently own!
+Want to add your favorite book? Simply find your favorite reading material's ISBN and add it to EzBookXChange to have a customized platform to note the books you currently own!
 
 <p align="center">
 <img src="./images/findingBooks.png" />
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,69 +1,35 @@
-const React = require("react");
+const React = require('react');
 import { Link } from "react-router-dom";
 
 class Nav extends React.Component {
+
   render() {
     const navComponents = [];
     if (this.props.loggedIn) {
-      console.log(this.props);
+      console.log(this.props)
       navComponents.push(
         <ul key={0} className="nav-items">
-          <li key={0}>
-            <Link to="/">Home</Link>
-          </li>
-          <li key={1}>
-            <Link to="/mypage">My Books</Link>
-          </li>
-          <li key={2}>
-            <Link to="/exchange">My Requests</Link>
-          </li>
-          <li key={3}>
-            <Link to="/search">Find Books</Link>
-          </li>
-          <li key={4}>
-            <Link to="/" onClick={this.props.logOut}>
-              Log out
-            </Link>
-          </li>
+          <li key={0}><Link to="/">Home</Link></li>
+          <li key={1}><Link to="/mypage">My Books</Link></li>
+          <li key={2}><Link to="/exchange">My Requests</Link></li>
+          <li key={3}><Link to="/search">Find Books</Link></li>
+          <li key={4}><Link to="/" onClick={this.props.logOut}>Log out</Link></li>
         </ul>
-      );
+      )
     } else {
       navComponents.push(
-        <ul
-          key={1}
-          style={{
-            alignItems: "center",
-            justifyContent: "space-evenly",
-            display: "flex",
-          }}
-        >
-          <li key={1}>
-            <Link
-              to="/login"
-              style={{
-                fontSize: "20px",
-                textDecoration: "bold",
-              }}
-            >
-              Login
-            </Link>
-          </li>
-          <li key={2}>
-            <Link
-              to="/register"
-              style={{
-                fontSize: "20px",
-                textDecoration: "bold",
-              }}
-            >
-              Register
-            </Link>
-          </li>
+        <ul key={1}>
+          <li key={1}><Link to="/login">Login</Link></li>
+          <li key={2}><Link to="/register">Register</Link></li>
         </ul>
-      );
+      )
     }
 
-    return <div className="nav-bar">{navComponents}</div>;
+    return (
+      <div className="nav-bar">
+        {navComponents}
+      </div>
+    )
   }
 }
 
